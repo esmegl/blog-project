bc542305c58f93a24d0f597e07562ce5fefd4d9a
Esmeralda Gallardo <gallardo.esmeralda05@gmail.com>
2022-10-16 11:10:45 -0300

    Created blog and category models

--- main/models.py
+++ main/models.py
@@ -8,9 +8,12 @@ class Category(models.Model):
 
 class Blog(models.Model):
 	category = models.ForeignKey(Category, on_delete=models.CASCADE)
+	date = models.DateTimeField(auto_now_add=True)
 	title = models.CharField(max_length=250)
 	description = models.TextField(max_length=2000)
 	imageURL = models.URLField(null=True, blank=True, max_length=2000)
+	# comments = models.CharField(max_length=2000)
+	# stars = models.IntegerField()
 
 	def __str__(self) -> str:
 		return self.title
