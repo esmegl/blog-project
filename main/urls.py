09e7fd469508098ac77df7a4ccd7ea124501d5b5
Esmeralda Gallardo <gallardo.esmeralda05@gmail.com>
2022-10-16 11:13:49 -0300

    Added views and urls

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,8 @@ app_name = 'main'
 
 urlpatterns = [
 	path('', views.home, name='home'),
+	path('details/<int:id>/', views.details, name='details'),
+	path('add-blog/', views.add_blog, name='add_blog'),
+	path('edit-blog/<int:id>/', views.edit_blog, name='edit_blog'),
+	path('delete-blog/<int:id>/', views.delete_blog, name='delete_blog'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from .models import *
+from .forms import *
 
 def home(request):
-	context = {}
+	blogs = Blog.objects.all().order_by('-id')
+	context = {
+		'blogs': blogs
+	}
 	return render(request, 'main/index.html', context)
+
+
+def details(request, id):
+	blog = Blog.objects.get(id=id)
+	context = {
+		'blog': blog
+	}
+	return render(request, 'main/details.html', context)
+
+def add_blog(request):
+	if request.user.is_authenticated:
+		if request.method == 'POST':
+			form = BlogForm(request.POST or None)
+			if form.is_valid():
+				data = form.save()
+				return redirect('main:home')
+		else:
+			form = BlogForm()
+
+		return render(request, 'main/add_blog.html', {'form': form})
+	else:
+		return redirect('main:home')
+
+def edit_blog(request, id):
+	if request.user.is_authenticated:
+		blog = Blog.objects.get(id=id)
+		if request.method == 'POST':
+			form = BlogForm(request.POST, instance=blog)
+			if form.is_valid():
+				data = form.save()
+				return redirect('main:details', id)
+		else:
+			form = BlogForm(instance=blog)
+
+		return render(request, 'main/add_blog.html', {'form': form})
+	else:
+		return redirect('main:home')
+
+def delete_blog(request, id):
+	if request.user.is_authenticated:
+		blog = Blog.objects.get(id=id)
+		blog.delete()
+		return redirect('main:home')
+	else:
+		return redirect('main:details', id)
